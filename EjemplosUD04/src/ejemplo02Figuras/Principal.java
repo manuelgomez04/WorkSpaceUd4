6a2125c5ced08798380055ff5f12d119096804e8
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-01-12 14:27:36 +0100

    Ejemplo 2

--- EjemplosUD04/src/ejemplo02Figuras/Principal.java
+++ EjemplosUD04/src/ejemplo02Figuras/Principal.java
@@ -5,9 +5,12 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Figura cuadrado = new Cuadrado(1, 2, 3);
+		Figura cuadrado = new Cuadrado(1, 2, 2);
+		Figura rectangulo = new Rectangulo(1, 2, 3, 4);
 
 		System.out.println(cuadrado.calcularArea());
+		System.out.println(rectangulo.calcularArea());
+
 	}
 
 }
--- EjemplosUD04/src/ejemplo02Figuras/Rectangulo.java
+++ EjemplosUD04/src/ejemplo02Figuras/Rectangulo.java
@@ -1,12 +1,12 @@
 package ejemplo02Figuras;
 
-public class Rectangulo {
+public class Rectangulo extends Figura {
 
 	private double base;
 	private double altura;
 
-	public Rectangulo(double base, double altura) {
-		super();
+	public Rectangulo(double x, double y, double base, double altura) {
+		super(x, y);
 		this.base = base;
 		this.altura = altura;
 	}
