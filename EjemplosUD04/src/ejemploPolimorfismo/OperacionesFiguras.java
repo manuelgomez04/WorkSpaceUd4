62bfc7a1839eeb17fa5310267f3759fa5f6db752
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-01-16 13:39:58 +0100

    EOOOOEOEOEOEOEOEEOOEOEOEOEOEOEEOEOEOEOEOEOEOEOEOEOEOEOEOEOEOEOEOEOEO

--- EjemplosUD04/src/ejemploPolimorfismo/OperacionesFiguras.java
+++ EjemplosUD04/src/ejemploPolimorfismo/OperacionesFiguras.java
@@ -16,4 +16,40 @@ public class OperacionesFiguras {
 		return resultado;
 	}
 
+	// ESTE ES EL MÉTODO QUE NO USAREMOS, YA QUE DIGAMOS QUE NOS "SALTAMOS" UN PASO
+	// EN EL POLIMORFISMO. YA QUE ES MENOS GENÉRICA.
+
+	public double sumarAreasFiguras2(Figura[] lista) {
+
+		double suma = 0;
+
+		for (int i = 0; i < lista.length; i++) {
+			suma += lista[i].calcularArea();
+		}
+		return suma;
+	}
+
+	public double sumarAreaAviso(Figura[] lista) {
+
+		double resultado = 0;
+
+		for (int i = 0; i < lista.length; i++) {
+			resultado += calcularAreaFigura(lista[i]);
+
+			// EN ESTE CASO QUEREMOS QUE SI ES UN CÍRCULO, NOS MUESTRE EL MÉTODO MOSTRAR
+			// RADIANES, EXCLUSIVO DE LA CLASE CÍRCULO, TENEMOS QUE CASTEAR
+			// UNA FIGURA COMO UN CÍRCULO, PARA PODER ACCEDER AL MÉTODO.
+			if (lista[i] instanceof Circulo) {
+
+				// FORMA 1 (VOY A USAR ESTO)
+				((Circulo) lista[i]).mostrarRadianes();
+				// MEJOR NO USARLO
+				Circulo c = (Circulo) lista[i];
+				c.mostrarRadianes();
+			}
+		}
+		return resultado;
+
+	}
+
 }
--- EjemplosUD04/src/ejemploPolimorfismo/Principal.java
+++ EjemplosUD04/src/ejemploPolimorfismo/Principal.java
@@ -4,9 +4,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 
+		int tam = 2;
+		Figura lista[] = new Figura[tam];
+
 		Figura cua = new Cuadrado("Cuadrado", "Azul", 2);
 		Figura cir = new Circulo("Círculo", "Rosa", 2.5);
 
+		// ESTO ES SOLO PPARA EL EJEMPLO
+		// CABROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOON
+		lista[0] = cua;
+		lista[1] = cir;
 		OperacionesFiguras op = new OperacionesFiguras();
 
 		System.out.printf("El área de un círculo es %.2f", op.calcularAreaFigura(cir));
@@ -18,6 +25,9 @@ public class Principal {
 		System.out.println();
 
 		cua.metodoSoloDeFigura();
+
+		System.out.println(op.sumarAreas(lista));
+		System.out.println(op.sumarAreasFiguras2(lista));
 
 		/*
 		 * CUANDO UN MÉTODO NO ES REESCRITO, SI EL OBJETO ES DEL TIPO DE LA MADRE,
